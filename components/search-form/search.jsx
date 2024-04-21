665270dd27525dd80b8f96f961b51a3958937b60
NeoDitjoe <125878968+NeoDitjoe@users.noreply.github.com>
2024-04-21 17:29:27 +0200

    replace logo

--- components/search-form/search.jsx
+++ components/search-form/search.jsx
@@ -5,11 +5,12 @@ export default function Search() {
   return (
     <form>
       <div className='flex flex-row items-center m-4 justify-center'>
-        <div className='bg-gray-200 p-5 border-b border-black'>
-          <FaSearch background='gray' size={24} />
-        </div>
         <div className='w-80 bg-gray-200 p-5 border-b border-black'>
           <input style={{ all: 'unset' }} type='text' name='search' placeholder='What are you look for ?' required />
+        </div>
+
+        <div className='bg-gray-200 p-5 border-b border-black'>
+          <FaSearch background='gray' size={24} />
         </div>
       </div>
     </form>

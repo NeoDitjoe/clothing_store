5b65b97cac464674c6514811de78b20bc7e00d0f
NeoDitjoe <125878968+NeoDitjoe@users.noreply.github.com>
2024-04-21 17:44:07 +0200

    search styling

--- components/search-form/search.jsx
+++ components/search-form/search.jsx
@@ -1,17 +1,25 @@
 import React from 'react'
 import { FaSearch } from 'react-icons/fa'
+import styles from './search.module.css'
 
 export default function Search() {
+
+  async function searchHandler(e){
+    e.preventDefault()
+    alert('Being up dated')
+  }
   return (
-    <form>
+    <form onSubmit={searchHandler} className={styles.form}>
       <div className='flex flex-row items-center m-4 justify-center'>
-        <div className='w-80 bg-gray-200 p-5 border-b border-black'>
-          <input style={{ all: 'unset' }} type='text' name='search' placeholder='What are you look for ?' required />
+        <div className='bg-gray-200'>
+          <input type='text' name='search' placeholder='What are you look for ?' required />
         </div>
 
-        <div className='bg-gray-200 p-5 border-b border-black'>
-          <FaSearch background='gray' size={24} />
-        </div>
+        <button>
+          <div className='bg-gray-300 p-5 border-b border-black hover:bg-gray-200 focus:bg-gray-200 cursor-pointer'>
+            <FaSearch background='gray' size={24} />
+          </div>
+        </button>
       </div>
     </form>
   )

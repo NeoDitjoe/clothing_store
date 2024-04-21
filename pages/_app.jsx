9391aa23a5e842663a7a37e2c8104fde5e11cdfa
NeoDitjoe <125878968+NeoDitjoe@users.noreply.github.com>
2024-04-21 13:27:36 +0200

    install tailwind and next ui

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,12 +1,17 @@
+'use client'
+
 import "@/styles/globals.css";
+import { NextUIProvider } from "@nextui-org/react";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
   return (
-    <div className={inter.className}>
-      <Component {...pageProps} />
-    </div>
+    <NextUIProvider>
+      <div className={inter.className}>
+        <Component {...pageProps} />
+      </div>
+    </NextUIProvider>
   );
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,12 @@
-import Image from "next/image";
-
+import {Button} from "@nextui-org/react";
 
 export default function Home() {
   return (
     <main>
       <h1>Hello World</h1>
+      <Button color="primary">
+      Button
+    </Button>
     </main>
   );
 }

877f05043ccd5ed1ad32fbd8005bb06bfe72fd5f
NeoDitjoe <125878968+NeoDitjoe@users.noreply.github.com>
2024-04-23 13:31:34 +0200

    clear selected item when selected item is clicked

--- components/all-products/select-filters-options/filter-options.jsx
+++ components/all-products/select-filters-options/filter-options.jsx
@@ -5,11 +5,20 @@ export default function FilterOptions(props) {
   const { categories } = props
   const router = useRouter()
 
-  const filter = router.query.q + ' '
+  let filter = router.query.q + ' '
   let filterby = ''
   filterby += filter
 
   const isSelected = filter.split(' ')
+
+  /**
+   * when all Item were removed the query q was left behind with empty
+   * an empty string. this is what was left = { products?p=1&q=[''] }
+   * 
+   * So when we remove the last select item we completeley remove 
+   * the query q buy pushing { products?p=1 } to our url.
+   */
+  let clearQuery = false
 
   return (
     <main className='flex justity-center bg-white p-8 max-w-[60em]'>
@@ -20,14 +29,30 @@ export default function FilterOptions(props) {
               onClick={() => {
                 
                 if(!isSelected.includes(item)){
-                  filterby += item + ' '
+                  router.query.q 
+                  ? filterby += item + ' '
+                  : filterby = item + ' '
+                }else{
+                  const removeItem = isSelected.filter(selected => selected !== item)
+
+                  if(isSelected.length == 2){
+                    clearQuery = true
+                  }else{
+                    filterby = removeItem.join(' ').trim()
+                    clearQuery = false
+                  }
+
                 }
 
                 setTimeout(() => {
-                  router.push(`/products?p=1&q=${filterby}`)
+
+                  !clearQuery 
+                  ? router.push(`/products?p=1&q=${filterby}`)
+                  : router.push('/products?p=1')
+
                 }, 200);
               }}
-              className={`border border-pink-400 border-solid border-1 rounded pl-2 pr-2 hover:bg-pink-400  hover:text-white transition-colors duration-300 ease-in-out cursor-pointer ${isSelected.includes(item) && 'bg-pink-400 text-white'}` }
+              className={`border border-pink-400 border-solid border-1 rounded pl-2 pr-2 hover:bg-pink-300  hover:text-white transition-colors duration-300 ease-in-out cursor-pointer ${isSelected.includes(item) && 'bg-pink-400 text-white'}` }
             >{item}</div>
           ))
         }
@@ -35,3 +60,7 @@ export default function FilterOptions(props) {
     </main>
   )
 }
+
+/**
+ * if array only one option remove the q its self
+ */

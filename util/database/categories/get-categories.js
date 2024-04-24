bffde7fefc7efcf8df1cf9f1e0194450dc535d08
NeoDitjoe <125878968+NeoDitjoe@users.noreply.github.com>
2024-04-24 06:15:29 +0200

    fetch unique categories using pipeline to improve performace

--- util/database/categories/get-categories.js
+++ util/database/categories/get-categories.js
@@ -1,18 +1,39 @@
-import client from '../connect-mongo-client'
+import client from '../connect-mongo-client';
 
 export default async function getCategories() {
-  
-  const db = client.db('store')
 
-  const data = await db.collection('items')
-    .aggregate([
-      { $project: { _id: 0, categories: 1 } }
-    ])
-    .toArray()
+  const db = client.db('store');
 
-  const categories = data.map((item) => item.categories)
-  const uniqueCategories = new Set(categories.flat())
+  const pipeline = [
+    {
+      $project: {
+        _id: 0,
+        categories: 1
+      }
+    },
+    {
+      $unwind: '$categories'
+    },
+    {
+      $group: {
+        _id: null,
+        uniqueCategories: { $addToSet: '$categories' }
+      }
+    },
+    {
+      $project: {
+        _id: 0,
+        categories: '$uniqueCategories'
+      }
+    }
+  ];
 
-  return [...uniqueCategories]
+  const result = await db.collection('items').aggregate(pipeline).toArray();
+
+  const categories = result.length > 0 ? result[0].categories : [];
+
+  return categories;
+
 }
+
 

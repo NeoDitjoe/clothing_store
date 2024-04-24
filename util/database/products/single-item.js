5435726384be30d58abe45a518a1f4e444e09f02
NeoDitjoe <125878968+NeoDitjoe@users.noreply.github.com>
2024-04-24 06:27:20 +0200

    get only needed data

--- util/database/products/single-item.js
+++ util/database/products/single-item.js
@@ -2,21 +2,27 @@ import React from 'react'
 import client from '../connect-mongo-client'
 
 export default async function singleItem(id) {
-  
+
   const db = client.db('store')
 
   const item = await db.collection('items')
     .aggregate([
-      {$match: {id}},
-      {$project: {_id: 0}}
+      { $match: { id } },
+      {
+        $project: {
+          _id: 0, image: 1, name: 1,
+          price: 1, brand: 1, description: 1,
+          size: 1, material: 1, categories: 1
+        }
+      }
     ])
     .toArray()
 
-    const getCategories = item.map((item) => item.categories)
-    const categories = getCategories[0].slice(0, 3)
+  const getCategories = item.map((item) => item.categories)
+  const categories = getCategories[0].slice(0, 3)
 
-    return {
-      item, 
-      categories
-    }
+  return {
+    item,
+    categories
+  }
 }

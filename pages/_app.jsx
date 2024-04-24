082ebbf4f22571d87929a8f6054fd10e93bb0e81
NeoDitjoe <125878968+NeoDitjoe@users.noreply.github.com>
2024-04-24 07:08:39 +0200

    move insight to app

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -4,6 +4,7 @@ import Layout from "@/components/layout/layout";
 import "@/styles/globals.css";
 import { NextUIProvider } from "@nextui-org/react";
 import { Inter } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,6 +13,7 @@ export default function App({ Component, pageProps }) {
     <NextUIProvider>
       <Layout className={inter.className}>
         <Component {...pageProps} />
+        <SpeedInsights />
       </Layout>
     </NextUIProvider>
   );
